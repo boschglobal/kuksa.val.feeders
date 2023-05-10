504bba19bebcccb30d9ea7c7ce6b0c604e6bfade
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2023-05-10 19:46:20 +0300

    spdx
    exclude cert/ from flake8
    
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- someip2val/cert/__init__.py
+++ someip2val/cert/__init__.py
@@ -1,5 +1,18 @@
+#################################################################################
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License 2.0 which is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#################################################################################
+
 import os
 
 from kuksa_client._metadata import *
 
-__certificate_dir__= os.path.dirname(os.path.realpath(__file__))
+__certificate_dir__ = os.path.dirname(os.path.realpath(__file__))
--- someip2val/cert/getCerts.sh
+++ someip2val/cert/getCerts.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+#################################################################################
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License 2.0 which is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#################################################################################
+
 
 genCAKey() {
     openssl genrsa -out CA.key 2048
@@ -55,4 +68,3 @@ do
     genCert $i
     echo ""
 done
-
